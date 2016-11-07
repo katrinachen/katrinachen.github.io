29fdebb1ab335e79894579c6dd56161c73d98ecb
Kaushiksudhir <kaushik.nt@hotmail.com>
2016-11-08 04:52:21 +0800

    res

---
+++ learn/learn.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+//
+ $(document).ready(function() {
+        $('button').click(function(e) { 
+//        	var target = e.target || e.srcElement;
+        	var className=e.target.getAttribute("class");
+    		var x=document.getElementById(className);
+        	//turn show to hide and vice versa
+        	
+        	if(e.target.innerText=="Show"){
+        		e.target.innerText="Hide";
+        		x.innerText = "b";
+        		        		
+        	}else if(e.target.innerText=="Hide"){
+        		e.target.innerText="Show";
+        		x.innerText="";        		
+        	}else if(e.target.innerText=="Show All"){        		
+        		document.getElementById("showall").disabled = true;
+        		document.getElementById("clearall").disabled = false;
+        		
+        		
+        	}else if(e.target.innerText=="Clear All"){
+        		document.getElementById("showall").disabled = false;
+        		document.getElementById("clearall").disabled = true;
+        		
+        	}
+        		
+//        	alert(e.target.getAttribute("class"));
+//           alert(e.target.innerText);
+           // return false;
+        });
+    });
+
+//$(document).ready(function(){
+// alert("This is Hello World by JQuery");
+//});
---
+++ my.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+function myfun(){
+	alert("h");
+var text = '{"name":"John Johnson","street":"Oslo West 16","phone":"555 1234567"}';
+
+var obj = JSON.parse(text);
+
+document.getElementById("demo").innerHTML =
+obj.name + "<br>" +
+obj.street + "<br>" +
+obj.phone;
+}
